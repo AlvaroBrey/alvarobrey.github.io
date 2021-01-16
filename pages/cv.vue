43c13dc298eb405af6f19d5d30b71585f141f89d
Álvaro Brey <alvaro.brv@gmail.com>
2021-01-16 01:07:20 +0100

    cv: Update CV pdf link and imagames description

--- pages/cv.vue
+++ pages/cv.vue
@@ -8,7 +8,7 @@
       />
       <i18n path="page.cv.introduction" tag="p" class="px-3">
         <a
-          href="/downloads/AlvaroBreyVilas_CV_en.pdf"
+          href="/files/AlvaroBreyVilas-cv-web.pdf"
           download
           target="_blank"
           v-text="$t('page.cv.introductionLinkHere')"
