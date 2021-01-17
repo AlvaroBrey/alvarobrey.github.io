d657134e34bcab9980f8dbe15322703710a5032b
Álvaro Brey <alvaro.brv@gmail.com>
2021-01-17 15:38:37 +0100

    Implement contact page

---
+++ components/contact/ContactItemDisplay.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-col cols="12" xs="12" sm="6" lg="3" align="center" justify="center">
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on }">
+        <v-hover v-slot="{ hover }">
+          <a
+            v-ripple
+            :href="link"
+            target="_blank"
+            style="display: block"
+            v-on="on"
+          >
+            <v-icon size="120" :color="hover ? 'primary' : ''">
+              {{ icon }}
+            </v-icon>
+            <p class="text-subtitle-1">
+              {{ $t(`page.contact.item.${translationKey}`) }}
+            </p>
+          </a>
+        </v-hover>
+      </template>
+      <span>{{ formattedTooltip }}</span>
+    </v-tooltip>
+  </v-col>
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue'
+
+export default Vue.extend({
+  props: {
+    icon: {
+      type: String,
+      required: true
+    } as PropOptions<string>,
+    translationKey: {
+      type: String,
+      required: true
+    } as PropOptions<string>,
+    link: {
+      type: String,
+      required: true
+    } as PropOptions<string>
+  },
+  computed: {
+    formattedTooltip(): string {
+      return this.link.replace('https://', '').replace('mailto:', '')
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+a {
+  outline: none;
+  &::-moz-focus-inner {
+    border: 0;
+  }
+}
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,60 @@
+<template>
+  <BasicPage :title="$t('page.contact.title')">
+    <p class="text-body-1">
+      {{ $t('page.contact.intro') }}
+    </p>
+    <v-row justify="center" align="center">
+      <ContactItemDisplay
+        v-for="item in items"
+        :key="item.key"
+        :translation-key="item.key"
+        :link="item.link"
+        :icon="item.icon"
+      />
+    </v-row>
+  </BasicPage>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import BasicPage from '~/components/BasicPage.vue'
+import ContactItemDisplay from '~/components/contact/ContactItemDisplay.vue'
+
+interface ContactItem {
+  key: string
+  icon: string
+  link: string
+}
+
+const items: ContactItem[] = [
+  {
+    key: 'linkedIn',
+    icon: 'mdi-linkedin',
+    link: 'https://linkedin.com/in/alvarobrey'
+  },
+  {
+    key: 'gitHub',
+    icon: 'mdi-github',
+    link: 'https://github.com/alvarobrey'
+  },
+  {
+    key: 'twitter',
+    icon: 'mdi-twitter',
+    link: 'https://twitter.com/AlvaroBrey_'
+  },
+  {
+    key: 'mail',
+    icon: 'mdi-email',
+    link: 'mailto:alvaro.brv@gmail.com'
+  }
+]
+
+export default Vue.extend({
+  components: { ContactItemDisplay, BasicPage },
+  data() {
+    return {
+      items
+    }
+  }
+})
+</script>
