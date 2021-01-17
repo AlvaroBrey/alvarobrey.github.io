17cf80234002cd6878d2400c7b5afcf2713c899d
Álvaro Brey <alvaro.brv@gmail.com>
2021-01-17 17:18:40 +0100

    Implement basic about page

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- eslint-disable vue/no-v-html -->
+  <BasicPage :title="$t('page.about.title')">
+    <p class="text-body-1 mt-2" v-html="$t('page.about.paragraph1')" />
+    <p class="text-body-1 mt-3" v-html="$t('page.about.paragraph2')" />
+  </BasicPage>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import BasicPage from '~/components/BasicPage.vue'
+
+export default Vue.extend({
+  components: { BasicPage }
+})
+</script>
