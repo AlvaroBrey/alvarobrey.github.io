3715e79edb3b52b59b21d62695bede6c1172b94e
Álvaro Brey <alvaro.brv@gmail.com>
2021-12-15 23:27:22 +0100

    Update pdf CV

--- pages/cv.vue
+++ pages/cv.vue
@@ -8,7 +8,7 @@
         <v-btn
           class="secondary--text"
           color="primary"
-          href="/files/AlvaroBreyVilas-cv-20210721.pdf"
+          href="/files/AlvaroBreyVilas-cv-20211215.pdf"
           download
           target="_blank"
         >
