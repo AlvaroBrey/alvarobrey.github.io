8da88282c452ea100b087fe95cc79ff7ea9faf84
Álvaro Brey <alvaro@alvarobrey.com>
2022-08-01 23:57:21 +0200

    Change color palette
    old one was boring

--- vuetify.options.ts
+++ vuetify.options.ts
@@ -2,16 +2,14 @@ import { ThemeOptions, VuetifyThemeVariant } from 'vuetify/types/services/theme'
 import { GlobalVuetifyPreset } from 'vuetify/types/services/presets'
 
 const lightTheme: Partial<VuetifyThemeVariant> = {
-  primary: '#4a4e69',
-  secondary: '#fcf9f8',
-  accent: '#C9ADA7'
+  primary: '#388E3C',
+  secondary: '#eff3ef'
 }
 
 // TODO tweak dark theme and enable switching in app
 const darkTheme: Partial<VuetifyThemeVariant> = {
-  primary: '#c9ada7',
-  secondary: '#4A4E69',
-  accent: '#C9ADA7'
+  primary: '#4CAF50',
+  secondary: '#3b3b49'
 }
 
 const themeOptions: ThemeOptions = {
