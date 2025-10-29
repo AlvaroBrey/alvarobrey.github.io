11a0e91b3ecbbe483b1053c90a024c21bccc61cb
AlvaroBrey <AlvaroBrey@users.noreply.github.com>
2025-10-29 21:36:32 +0000

    deploy: 91dd0967b1b95dcc8f135cf9f5980a5e2fb16228

---
+++ _nuxt/static/1761773791/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fes\u002Fabout","\u002Fcv","\u002Fme","\u002Fabout","\u002Fes\u002Fme","\u002Fes\u002Fcv","\u002Fcontact","\u002Fes\u002Fcontact","\u002Fes"]})
