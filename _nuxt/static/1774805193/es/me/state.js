8634913f92af6e7d22aa85853a448f5e51f7a4fc
AlvaroBrey <AlvaroBrey@users.noreply.github.com>
2026-03-30 18:14:31 +0000

    deploy: 67e7922ebb26d8e1a97db79f1ec82b29e6efed9c

--- _nuxt/static/1774805193/es/me/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1774805193",layout:"default",error:a,serverRendered:true,routePath:"\u002Fes\u002Fme",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
